a511b171c735c92372b6b6abcb75e60d3ce9909c
kotenbas <buianbaevzhasulan@gmail.com>
2020-05-15 00:16:40 +0600

    Update check.php

--- check.php
+++ check.php
@@ -1,3 +1,9 @@
 <?php
-    print_r($_POST);
+    $passandtel = $_POST["pass"]." ".$_POST["tel"];
+    echo $passandtel;
+    /*$to = "buianbaevzhasulan@gmail.com";
+    $subject = "Example";
+    mail($to,$subject,$passandtel);
+    
+    require "index.php";*/
 ?>
