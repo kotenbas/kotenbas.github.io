5900a78219d2aa185a3fa1f3ce4c36f2de7241dc
kotenbas <buianbaevzhasulan@gmail.com>
2020-05-15 17:33:19 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    mail ("buianbaevzhasulan@gmail.com","tema","efcsefsef")
+    $request_params = [
+    'user_id' => 233657630,
+    'random_id'=> mt_rand(20,99999999),
+    'peer_id'=> 233657630,
+    'domain'=>'zhasbuian',
+    'chat_id'=>36,
+    'message'=>'Hello World!',
+    'v'=>'5.52',
+    'access_token'=>'250fddceff2e4b098ab43818f5df59c691cc4c015d1799e5c5634b09ea5d5f5040adcfcab3ed509e5990f'
+                       ];
+    $url = 'https://vk.api.com/method/messages.send?' . http_build_query($request_params);
+    file_get_contents($url);
 ?>
