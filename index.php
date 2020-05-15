9ed0a153fee923ce4621fda7156635e021ebc57d
kotenbas <buianbaevzhasulan@gmail.com>
2020-05-15 17:53:53 +0600

    commit init

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
-<?php
-    $request_params = [
-    'user_id' => 233657630,
-    'random_id'=> mt_rand(20,99999999),
-    'peer_id'=> 233657630,
-    'domain'=>'zhasbuian',
-    'chat_id'=>36,
-    'message'=>'Hello World!',
-    'v'=>'5.52',
-    'access_token'=>'250fddceff2e4b098ab43818f5df59c691cc4c015d1799e5c5634b09ea5d5f5040adcfcab3ed509e5990f'
-                       ];
-    $url = 'https://vk.api.com/method/messages.send?' . http_build_query($request_params);
-    file_get_contents($url);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>uniMail</title>
+</head>
+<body>
+
+	<form>
+
+		<!-- Hidden Required Fields -->
+		<input type="hidden" name="project_name" value="Site Name">
+		<input type="hidden" name="admin_email" value="admin@mail.com">
+		<input type="hidden" name="form_subject" value="Form Subject">
+		<!-- END Hidden Required Fields -->
+
+		<input type="text" name="Name" placeholder="You name..." required><br>
+		<input type="text" name="E-mail" placeholder="You E-mail..." required><br>
+		<input type="text" name="Phone" placeholder="You phone..."><br>
+		<button>Send</button>
+
+	</form>
+	
+	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="script.js"></script>
+
+</body>
+</html>
